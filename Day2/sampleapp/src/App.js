e14717a53b4f9c25583ebe5b6e749ce21725bb5c
SUREN S <surenmanju2004@gmail.com>
2023-12-09 16:13:54 +0530

    Day2

---
+++ Day2/sampleapp/src/App.js
@@ -0,0 +1,18 @@
+import "./Assets/CSS/styles.css"
+function App(){
+    return(
+       
+        <div class="login">
+        <h2 id="formTitle">Register Here</h2>
+        <form className="form1" id="loginForm" onsubmit="return validateForm()">
+            <label>Name</label> <input type="text" name="name" id="name" required/>
+            <label>Reg No</label> <input type="text" name="regno" id="regno" required/>
+            <label>Email</label> <input type="email" name="mail" id="mail" required/>
+            <label>Password</label> <input type="password" name="pwd" id="pwd" required/>
+            <label>Confirm Password</label> <input type="password" name="pwd1" id="pwd1" required/>
+            <button type="submit">Submit</button>
+        </form>
+        </div>
+    )
+}
+export default App
